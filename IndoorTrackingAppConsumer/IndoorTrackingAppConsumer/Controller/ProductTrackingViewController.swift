cc7e59f86e70c2c107d6b79a3e507c3f3a651f78
Apope1999 <Apope1999@gmail.com>
2020-05-10 13:50:29 +0300

    Edited the location messages on the search product view

--- IndoorTrackingAppConsumer/IndoorTrackingAppConsumer/Controller/ProductTrackingViewController.swift
+++ IndoorTrackingAppConsumer/IndoorTrackingAppConsumer/Controller/ProductTrackingViewController.swift
@@ -28,6 +28,8 @@ class ProductTrackingViewController: UIViewController {
         let proximityUUID = UUID(uuidString:"98374d0a-fa8f-43ab-968b-88eaf83c6e4c")!
         let regionConstraint = CLBeaconIdentityConstraint(uuid: proximityUUID)
         locationManager.startRangingBeacons(satisfying: regionConstraint)
+        
+        metersLabel.minimumScaleFactor = 0.5
     }
 }
 
@@ -39,7 +41,20 @@ extension ProductTrackingViewController: CLLocationManagerDelegate {
             let minor = minorMajorArray?.first
             let major = minorMajorArray?.last
             if UInt16(truncating: beacon.minor) == minor && UInt16(truncating: beacon.major) == major {
-                metersLabel.text = "\(beacon.accuracy) M"
+                switch beacon.accuracy {
+                case -1:
+                    break
+                case 0..<5:
+                    metersLabel.text = "Less than 5 meters"
+                case 5..<10:
+                    metersLabel.text = "Less than 10 meters"
+                case 10..<20:
+                    metersLabel.text = "~15 meters"
+                case 20..<30:
+                    metersLabel.text = "~25 meters"
+                default:
+                    break
+                }
             }
         }
     }
